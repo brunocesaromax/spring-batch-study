a6bb6a5ee7c3b60256d77c5b36c8c79131902240
bruno-cesar <brunocesar.oc96@gmail.com>
2025-01-22 20:51:01 -0300

    Section 5 Class 35: Delegate Pattern with Spring Batch reader

--- ArquivoMultiplosFormatosJob/src/main/java/com/springbatch/arquivomultiplosformatos/dominio/Cliente.java
+++ ArquivoMultiplosFormatosJob/src/main/java/com/springbatch/arquivomultiplosformatos/dominio/Cliente.java
@@ -1,10 +1,14 @@
 package com.springbatch.arquivomultiplosformatos.dominio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
 	private String nome;
 	private String sobrenome;
 	private String idade;
 	private String email;
+	private List<Transacao> transacoes = new ArrayList<>();
 
 	public String getNome() {
 		return nome;
@@ -30,17 +34,27 @@ public class Cliente {
 		this.idade = idade;
 	}
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	@Override
-	public String toString() {
-		return "Cliente{" + "nome='" + nome + "'" + ", sobrenome ='" + sobrenome + "'" + ", idade='" + idade + "'"
-				+ ", email='" + email + "'" + '}';
-	}
+    public List<Transacao> getTransacoes() {
+        return transacoes;
+    }
+
+    public void setTransacoes(List<Transacao> transacoes) {
+        this.transacoes = transacoes;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nome='" + nome + "'" + ", sobrenome ='" + sobrenome + "'" + ", idade='" + idade + "'"
+                + ", email='" + email + "'" +
+                (transacoes.isEmpty() ? "" : ", transacoes=" + transacoes) +
+                '}';
+    }
 }
