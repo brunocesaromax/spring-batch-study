409441dafc541efecdbaf0247bb71ab27b6b77f5
bruno-cesar <brunocesar.oc96@gmail.com>
2025-01-22 21:14:52 -0300

    Section 5 Class 36: Read multiple files

--- ArquivoMultiplosFormatosJob/src/main/java/com/springbatch/arquivomultiplosformatos/step/LeituraArquivoMultiplosFormatosStepConfig.java
+++ ArquivoMultiplosFormatosJob/src/main/java/com/springbatch/arquivomultiplosformatos/step/LeituraArquivoMultiplosFormatosStepConfig.java
@@ -1,10 +1,10 @@
 package com.springbatch.arquivomultiplosformatos.step;
 
-import com.springbatch.arquivomultiplosformatos.reader.ArquivoClienteTransacaoReader;
+import com.springbatch.arquivomultiplosformatos.dominio.Cliente;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.MultiResourceItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +16,12 @@ public class LeituraArquivoMultiplosFormatosStepConfig {
 
 	@Bean
 	public Step leituraArquivoMultiplosFormatosStep(
-			FlatFileItemReader leituraArquivoMultiplosFormatosReader,
+			MultiResourceItemReader<Cliente> multiplosArquivoClienteTransacaoReader,
 			ItemWriter leituraArquivoMultiplosFormatosItemWriter) {
 		return stepBuilderFactory
 				.get("leituraArquivoMultiplosFormatosStep")
 				.chunk(1)
-				.reader(new ArquivoClienteTransacaoReader(leituraArquivoMultiplosFormatosReader))
+				.reader(multiplosArquivoClienteTransacaoReader)
 				.writer(leituraArquivoMultiplosFormatosItemWriter)
 				.build();
 	}
